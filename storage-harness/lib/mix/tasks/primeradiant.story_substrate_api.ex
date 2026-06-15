e8b529ecd2f20ab8a1ba16584af793c6e18243ed
Mike Manzano <mike@clicketyclacks.co>
2026-06-14 18:00:38 -0700

    Expose story substrate bind address

--- storage-harness/lib/mix/tasks/primeradiant.story_substrate_api.ex
+++ storage-harness/lib/mix/tasks/primeradiant.story_substrate_api.ex
@@ -16,7 +16,8 @@ defmodule Mix.Tasks.Primeradiant.StorySubstrateApi do
           tenant: :string,
           token: :string,
           ack_log: :string,
-          port: :integer
+          port: :integer,
+          ip: :string
         ]
       )
 
@@ -27,17 +28,25 @@ defmodule Mix.Tasks.Primeradiant.StorySubstrateApi do
     token = Keyword.fetch!(opts, :token)
     ack_log = Keyword.fetch!(opts, :ack_log)
     port = Keyword.get(opts, :port, 4084)
+    ip = opts |> Keyword.get(:ip, "127.0.0.1") |> parse_ip!()
 
     {:ok, _pid} =
       Plug.Cowboy.http(
         Router,
         [state: {:durable_soup_db, soup_db, tenant}, token: token, ack_log_path: ack_log],
         ref: :"primeradiant_story_substrate_#{port}",
-        ip: {127, 0, 0, 1},
+        ip: ip,
         port: port
       )
 
     Mix.shell().info("story-substrate API listening on #{port}")
     Process.sleep(:infinity)
+  end
+
+  defp parse_ip!(ip) do
+    case :inet.parse_address(String.to_charlist(ip)) do
+      {:ok, parsed} -> parsed
+      {:error, :einval} -> raise ArgumentError, "invalid --ip #{inspect(ip)}"
+    end
   end
 end
